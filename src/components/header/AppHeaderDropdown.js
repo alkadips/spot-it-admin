ff41b2cb514835eb975d79b1b12d5eb5e2d06c27
Alka <rashinkaralka@gmail.com>
2022-07-20 18:16:58 +0530

    Added logout fuctionality

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -23,8 +23,15 @@ import {
 import CIcon from '@coreui/icons-react'
 
 import avatar8 from './../../assets/images/avatars/8.jpg'
+import { useNavigate } from 'react-router-dom';
 
 const AppHeaderDropdown = () => {
+  const navigate = useNavigate()
+  const signOut=()=>{
+  localStorage.clear()
+  navigate("/login")
+  
+  }
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0" caret={false}>
@@ -84,9 +91,9 @@ const AppHeaderDropdown = () => {
           </CBadge>
         </CDropdownItem>
         <CDropdownDivider />
-        <CDropdownItem href="#">
-          <CIcon icon={cilLockLocked} className="me-2" />
-          Lock Account
+        <CDropdownItem  onClick={signOut}>
+          <CIcon icon={cilLockLocked}  className="me-2" />
+          Logout
         </CDropdownItem>
       </CDropdownMenu>
     </CDropdown>
